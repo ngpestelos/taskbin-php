c8057a236a7e507ff6848351c0ace8fb7c14a7e8
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-01-11 17:05:01 +0800

    simple tagging

--- tasks.py
+++ tasks.py
@@ -17,20 +17,26 @@ app = web.application(urls, locals(), autoreload=True)
 db = Server()['tasks']
 
 class Tag:
+    def append(self, tags, tag):
+        if tag in tags:
+            return tags
+        else:
+            tags.append(tag)
+            tags.sort()
+            return tags
+
     def POST(self):
         input = web.input()
         doc = db[input.task]
         if 'tags' in doc:
-            tags = doc['tags']
-            tags.append(input.tag)
-            tags.sort()
+            print "APPENDING"
+            tags = self.append(doc['tags'], input.tag)
             doc['tags'] = tags
         else:
             doc['tags'] = [input.tag]
-            print doc['tags']
-        db[input.task] = doc 
+        db[input.task] = doc
         raise web.seeother('/task/%s' % input.task)
-
+ 
 class Task:
     def GET(self, id):
         doc = db[id]
