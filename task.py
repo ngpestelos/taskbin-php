3f9e51f76856284337181c2b02bdeca4eb653210
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-02-22 23:07:07 +0800

    pre-defined views for tasks

--- task.py
+++ task.py
@@ -19,20 +19,72 @@ def post(stuff):
 def get(id):
     return db[id]
 
+#def getAll(type):
+#    ''' Returns a list of tasks for a given type.
+#        Sorts results by latest posting date. '''
+#    fun = '''
+#    function(doc) {
+#      if (doc.type == '%s' && doc.updated)
+#        emit(Date.parse(doc.updated), doc);
+#      else if (doc.type == '%s')
+#        emit(Date.parse(doc.posted), doc);
+#    }''' % (type, type)
+#    return [r.value for r in db.query(fun, descending=True)]
+
 def getAll(type):
     ''' Returns a list of tasks for a given type.
+
         Sorts results by latest posting date. '''
-    fun = '''
-    function(doc) {
-      if (doc.type == '%s' && doc.updated)
-        emit(Date.parse(doc.updated), doc);
-      else if (doc.type == '%s')
-        emit(Date.parse(doc.posted), doc);
-    }''' % (type, type)
-    return [r.value for r in db.query(fun, descending=True)]
+    def _tasks(type):
+        return [r.value for r in db.view('tasks/%s' % type, descending=True)]
+
+    if type == 'in':
+        return _tasks('in')
+    elif type == 'someday':
+        return _tasks('someday')
+    elif type == 'next':
+        return _tasks('next')
+    elif type == 'trash':
+        return _tasks('trash')
+    else:
+        return []
 
 def move(id, newtype):
     task = db[id]
     task['type'] = newtype
     task['updated'] = datetime.today().ctime()
     db[id] = task
+
+def create_views():
+    doc = {
+      "language": "javascript",
+      "views": {
+        "in": {
+          "map": """function(doc) {
+                      if (doc.type == 'in') emit(Date.parse(doc.posted), doc);
+                    }"""
+        },
+        "someday": {
+          "map": """function(doc) {
+                      if (doc.type == 'someday')
+                        emit(Date.parse(doc.posted), doc);
+                    }"""
+        },
+        "next": {
+          "map": """function(doc) {
+                      if (doc.type == 'next')
+                        emit(Date.parse(doc.posted), doc);
+                    }"""
+        },
+        "trash": {
+          "map": """function(doc) {
+                      if (doc.type == 'trash')
+                        emit(Date.parse(doc.posted), doc);
+                    }"""
+        }
+      }
+    }
+    db['_design/tasks'] = doc
+
+def delete_views():
+    del db['_design/tasks']
