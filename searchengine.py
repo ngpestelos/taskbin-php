91517657004537cd90e2a07915c994164f1ceb5a
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-02-24 18:46:34 +0800

    removed TooManyTerms exception

--- searchengine.py
+++ searchengine.py
@@ -3,18 +3,29 @@ from couchdb import Server
 index_db = Server()['taskbin_index']
 task_db = Server()['taskbin']
 
-class TooManyTerms(Exception):
-    """Exception raised when there are too many search terms"""
+def get_document(taskId):
+    return task_db[taskId]
 
 def find(words):
     q = words.split(' ')
-    if len(q) > 1:
-        raise TooManyTerms
-    try:
-        id = [r.id for r in index_db.view('index/words', key=q[0])][0]
-        word = index_db[id]
-        for taskId in word['tasks']:
-            task = task_db[taskId]
-            print (task['name'], task['_id'], task.get('tags', []), task['type'])
-    except IndexError:
-        print "None found."
+    firstrow = [r.id for r in index_db.view('index/words', key=q[0])]
+    if not firstrow:
+        return []
+
+    doc = index_db[firstrow[0]]
+    tasks = doc['tasks']
+    for n in q[1:]:
+        nextrow = [r.id for r in index_db.view('index/words', key=n)]
+        if not nextrow:
+            return []
+
+        nextdoc = index_db[nextrow[0]]
+        nexttasks = []
+        for task in tasks:
+            if task in nextdoc.get('tasks', []):
+                nexttasks.append(task)
+        tasks = nexttasks
+        if not tasks:
+            return []
+
+    return [get_document(id) for id in tasks]
