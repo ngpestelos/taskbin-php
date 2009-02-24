f52e94d29b4b282cf4d7703a6c1cbf920668a894
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-02-24 10:36:01 +0800

    very rough search

--- searchengine.py
+++ searchengine.py
@@ -1,14 +1,20 @@
 from couchdb import Server
 
-db = Server()['taskbin_index']
+index_db = Server()['taskbin_index']
+task_db = Server()['taskbin']
 
-def findDocIds(term):
-    fun = '''
-    function(doc) {
-      if (doc.word && doc.word == '%s')
-        emit(doc.docId, null);
-    }''' % term
-    return [r.key for r in db.query(fun)]
+class TooManyTerms(Exception):
+    """Exception raised when there are too many search terms"""
 
-if __name__ == '__main__':
-    print findDocIds('program')
+def find(words):
+    q = words.split(' ')
+    if len(q) > 1:
+        raise TooManyTerms
+    try:
+        id = [r.id for r in index_db.view('index/words', key=q[0])][0]
+        word = index_db[id]
+        for taskId in word['tasks']:
+            task = task_db[taskId]
+            print (task['name'], task['_id'], task.get('tags', []), task['type'])
+    except IndexError:
+        print "None found."
