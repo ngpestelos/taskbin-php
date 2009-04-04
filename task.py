41bc997b8c186dfd1f56f263989fba6ee577a16d
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-04-04 13:02:43 +0800

    removed unused code

--- task.py
+++ task.py
@@ -4,81 +4,10 @@ import tag
 
 db = Server()['taskbin']
 
-def comment(id, comment):
-    task = db[id]
-    comment = {'posted' : datetime.today().ctime(), 'comment' : comment}
-    task.setdefault('comments', []).append(comment)
-    task['updated'] = datetime.today().ctime()
-    db[id] = task
-
-def post(stuff):
-    task = dict(type='in', name=stuff['post'], posted=datetime.today().ctime())
-    id = db.create(task)
-    tag.post(id, stuff['tag'])
-
-def get(id):
-    return db[id]
-
-def getAll(type):
-    ''' Returns a list of tasks for a given type.
-
-        Sorts results by latest posting date. '''
-    def _tasks(type):
-        return [r.value for r in db.view('tasks/%s' % type, descending=True)]
-
-    if type == 'in':
-        return _tasks('in')
-    elif type == 'someday':
-        return _tasks('someday')
-    elif type == 'next':
-        return _tasks('next')
-    elif type == 'trash':
-        return _tasks('trash')
-    else:
-        return []
-
-def move(id, newtype):
-    task = db[id]
-    task['type'] = newtype
-    task['updated'] = datetime.today().ctime()
-    db[id] = task
-
-def create_views():
-    doc = {
-      "language": "javascript",
-      "views": {
-        "in": {
-          "map": """function(doc) {
-                      if (doc.type == 'in') emit(Date.parse(doc.posted), doc);
-                    }"""
-        },
-        "someday": {
-          "map": """function(doc) {
-                      if (doc.type == 'someday')
-                        emit(Date.parse(doc.posted), doc);
-                    }"""
-        },
-        "next": {
-          "map": """function(doc) {
-                      if (doc.type == 'next')
-                        emit(Date.parse(doc.posted), doc);
-                    }"""
-        },
-        "trash": {
-          "map": """function(doc) {
-                      if (doc.type == 'trash')
-                        emit(Date.parse(doc.posted), doc);
-                    }"""
-        },
-        "names": {
-          "map": """function(doc) {
-                      if (doc.name)
-                        emit(doc._id, doc.name);
-                    }"""
-        }
-      }
-    }
-    db['_design/tasks'] = doc
-
-def delete_views():
-    del db['_design/tasks']
+def post(task, tags):
+    task = {'type' : 'in', 'task' : task, 'posted' : datetime.today().ctime()}
+    taskId = db.create(task)
+    for t in tags.split(','):
+        tag = {'type' : 'tag', 'name' : t.strip(), \
+               'posted' : datetime.today().ctime(), 'task' : taskId}
+        db.create(tag)
