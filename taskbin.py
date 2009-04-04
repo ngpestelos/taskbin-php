13c3857f03bbbf52697b1927ad63824b2d61128f
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-04-04 10:09:05 +0800

    disabled most stuff for now

--- taskbin.py
+++ taskbin.py
@@ -2,107 +2,115 @@
 
 import web
 import task, tag
-import searchengine
+#import searchengine
+
+#urls = (
+#  '/t/(.*)/', 'Redirect',
+#  '/t/comment', 'Comment',
+#  '/t/tag/(.*)', 'TagDetail',
+#  '/t/tag', 'Tag',
+#  '/t/next', 'Next',
+#  '/t/someday', 'Someday',
+#  '/t/trash', 'Trash',
+#  '/t/move/(.*)', 'Move',
+#  '/t/task/(.*)', 'Detail',
+#  '/t/inbox', 'Inbox',
+#  '/t/search', 'Search',
+#  '/t/tags', 'Tags',
+#  '/t/new', 'Stuff',
+#  '/t', 'Index'
+#)
 
 urls = (
-  '/t/(.*)/', 'Redirect',
-  '/t/comment', 'Comment',
-  '/t/tag/(.*)', 'TagDetail',
-  '/t/tag', 'Tag',
-  '/t/next', 'Next',
-  '/t/someday', 'Someday',
-  '/t/trash', 'Trash',
-  '/t/move/(.*)', 'Move',
-  '/t/task/(.*)', 'Detail',
-  '/t/inbox', 'Inbox',
-  '/t/search', 'Search',
-  '/t/tags', 'Tags',
-  '/t/new', 'Stuff',
   '/t', 'Index'
 )
 
 app = web.application(urls, globals(), autoreload=True)
 
-render = web.template.render('static/', base='site')
-render_nobase = web.template.render('static/')
+#render = web.template.render('static/', base='site')
+render_bare = web.template.render('html')
 
-class Comment:
-    def POST(self):
-        input = web.input()
-        task.comment(input.task, input.comment)
-        raise web.seeother('/t/task/%s' % input.task)
+#class Comment:
+#    def POST(self):
+#        input = web.input()
+#        task.comment(input.task, input.comment)
+#        raise web.seeother('/t/task/%s' % input.task)
 
-class TagDetail:
-    def GET(self, id):
-        (t, tasks) = tag.getTasks(id)
-        return render.tag_detail(t, tasks)
+#class TagDetail:
+#    def GET(self, id):
+#        (t, tasks) = tag.getTasks(id)
+#        return render.tag_detail(t, tasks)
 
-class Next:
-    def GET(self):
-        return render.next(task.getAll('next'))
+#class Next:
+#    def GET(self):
+#        return render.next(task.getAll('next'))
 
-class Someday:
-    def GET(self):
-        return render.someday(task.getAll('someday'))
+#class Someday:
+#    def GET(self):
+#        return render.someday(task.getAll('someday'))
 
-class Trash:
-    def GET(self):
-        return render.trash(task.getAll('trash'))
+#class Trash:
+#    def GET(self):
+#        return render.trash(task.getAll('trash'))
 
-class Move:
-    def GET(self, id):
-        input = web.input()
-        if 'trash' in input:
-            task.move(id, 'trash')
-        elif 'someday' in input:
-            task.move(id, 'someday')
-        elif 'next' in input:
-            task.move(id, 'next')
-        raise web.seeother('/t')
+#class Move:
+#    def GET(self, id):
+#        input = web.input()
+#        if 'trash' in input:
+#            task.move(id, 'trash')
+#        elif 'someday' in input:
+#            task.move(id, 'someday')
+#        elif 'next' in input:
+#            task.move(id, 'next')
+#        raise web.seeother('/t')
 
-class Tag:
-    def POST(self):
-        input = web.input()
-        tag.post(input.task, input.tag)
-        raise web.seeother('/t/task/%s' % input.task)
+#class Tag:
+#    def POST(self):
+#        input = web.input()
+#        tag.post(input.task, input.tag)
+#        raise web.seeother('/t/task/%s' % input.task)
 
-class Detail:
-    def GET(self, id):
-        doc = task.get(id)
-        tags = [tag.get(name) for name in doc.get('tags', [])]
-        return render.task_detail(task.get(id), tags)
+#class Detail:
+#    def GET(self, id):
+#        doc = task.get(id)
+#        tags = [tag.get(name) for name in doc.get('tags', [])]
+#        return render.task_detail(task.get(id), tags)
 
-class Inbox:
-    def GET(self):
-        return render.inbox(task.getAll('in'))
+#class Inbox:
+#    def GET(self):
+#        return render.inbox(task.getAll('in'))
 
-class Redirect:
-    def GET(self, path):
-        raise web.seeother('/t' + path)
+#class Redirect:
+#    def GET(self, path):
+#        raise web.seeother('/t' + path)
 
-class Search:
-    def POST(self):
-        input = web.input()
-        results = searchengine.find(input.q)
-        tasks = [r for r in results if r['type'] != 'tag']
-        tags = [r for r in results if r['type'] == 'tag']
-        return render.search_results(input.q, tasks, tags)
+#class Search:
+#    def POST(self):
+#        input = web.input()
+#        results = searchengine.find(input.q)
+#        tasks = [r for r in results if r['type'] != 'tag']
+#        tags = [r for r in results if r['type'] == 'tag']
+#        return render.search_results(input.q, tasks, tags)
+
+#class Index:
+#    def GET(self):
+#        return render_nobase.index()
+
+#class Tags:
+#    def GET(self):
+#        return render.tags(tag.getAll())
+
+#class Stuff:
+#    def GET(self):
+#        return render.stuff()
+#    def POST(self):
+#        input = web.input()
+#        task.post(dict(post=input.task, tag=input.tag))
+#        raise web.seeother('/t')
 
 class Index:
     def GET(self):
-        return render_nobase.index()
-
-class Tags:
-    def GET(self):
-        return render.tags(tag.getAll())
-
-class Stuff:
-    def GET(self):
-        return render.stuff()
-    def POST(self):
-        input = web.input()
-        task.post(dict(post=input.task, tag=input.tag))
-        raise web.seeother('/t')
+        return render_bare.index()
 
 if __name__ == '__main__':
     app.run()
