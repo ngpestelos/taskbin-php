75e2a90492d99a525b4568a2e3ecf1136e2a6be9
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-04-08 20:51:45 +0800

    removed taskbin.js

--- design.py
+++ design.py
@@ -1,6 +1,38 @@
 from couchdb import Server
 import simplejson as json
 
+doc = {
+  'language' : 'javascript',
+  'views' : {
+    'inbox' : {
+      'map' : '''function(doc) {
+                   if (doc.type == 'inbox') emit(doc.posted, doc);
+                 }'''
+    },
+    'next'  : {
+      'map' : '''function(doc) {
+                   if (doc.type == 'next') emit(doc.posted, doc);
+                 }'''
+    },
+    'someday' : {
+      'map' : '''function(doc) {
+                   if (doc.type == 'someday') emit(doc.posted, doc);
+                 }'''
+    },
+    'trash' : {
+      'map' : '''function(doc) {
+                   if (doc.type == 'trash') emit(doc.posted, doc);
+                 }'''
+    }
+  }
+}
+
 def load():
     db = Server()['taskbin']
-    db['_design/taskbin'] = json.load(file('taskbin.js'))
+    if db.get('_design/taskbin'):
+        _doc = db['_design/taskbin']
+        db.delete(_doc)
+    db['_design/taskbin'] = doc
+
+if __name__ == '__main__':
+    load()
--- taskbin.js
+++
@@ -1,8 +0,0 @@
-{
-  "language" : "javascript",
-  "views"    : { 
-    "inbox"  : {
-      "map"  : "function(doc) {if (doc.type=='inbox') emit(doc.posted, doc);}"
-    }
-  }
-}
