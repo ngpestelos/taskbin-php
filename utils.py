663731b9f8c6ae3e48901e227cabe8e009adb643
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-04-11 11:58:30 +0800

    applying hashes to previously unhashed tags

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
 import hashlib
-
 from couchdb import Server
+import task
 
 db = Server()['taskbin']
 
-def apply_hashes(tags):
-    for t in tags:
-        id = t[0]
-        doc = t[1]
-        hash = hashlib.sha1(doc['name']).hexdigest()
-        doc['hash'] = hashlib.sha1(doc['name']).hexdigest()
+def get_tags():
+    fun = '''function(doc) {
+               if (doc.type == 'tag' && !doc.hash)
+                emit(doc._id, doc);
+             }'''
+    return [r.value for r in db.query(fun)]
+
+def apply_hashes():
+    tags = get_tags()
+    hashes = [(doc['_id'], hashlib.sha1(doc['name']).hexdigest()) \
+        for doc in tags]
+    for id, hash in hashes:
+        doc = db[id]
+        doc['hash'] = hash
         db[id] = doc
