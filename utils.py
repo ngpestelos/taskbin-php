6b92dce840bb9ee5c2a81536ec99f6e194cb674c
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-05-01 11:48:04 +0800

    unfortunately not working

--- utils.py
+++ utils.py
@@ -21,3 +21,15 @@ def apply_hashes():
         doc = db[id]
         doc['hash'] = hashed_text
         db[id] = doc
+
+def delete_tags():
+    d = [dict(_id=r.value['_id'], _rev=r.value['_rev'], _deleted=True) for r in db.view('_design/taskbin/_view/tags')]
+    to_delete = dict(docs=d)
+    import simplejson as json
+    import httplib
+    headers = {'Content-type' : 'application/json', 'Accept' : 'text/plain'}
+    conn = httplib.HTTPConnection('localhost:5984')
+    conn.request("POST", "/taskbin/_bulk_docs", json.dumps(d), headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    conn.close()
