b928c94ec454731eb4292d3cf6b55676c0365f6e
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-06-15 16:14:15 +0800

    links

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <ul>
       <li><a href=".">Home</a></li>
       <li><a href="tasks.php?type=inbox">Inbox</a></li>
-      <li>Next</li>
-      <li>Someday</li>
-      <li>Trash</li>
+      <li><a href="tasks.php?type=next">Next</a></li>
+      <li><a href="tasks.php?type=someday">Someday</a></li>
+      <li><a href="tasks.php?type=trash">Trash</a></li>
     </ul>
   </div>
 </div>
