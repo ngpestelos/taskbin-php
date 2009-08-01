2858744c9365782c484c9b322a995731482742b9
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-08-01 09:55:54 +0800

    merged nav with header

--- nav.php
+++
@@ -1,8 +0,0 @@
-<div id="nav" class="span-24">
-  <ul>
-    <li><a href="tasks.php?type=inbox">Inbox</a></li>
-    <li><a href="tasks.php?type=next">Next</a></li>
-    <li><a href="tasks.php?type=someday">Someday</a></li>
-    <li><a href="tasks.php?type=trash">Trash</a></li>
-  </ul>
-</div>
